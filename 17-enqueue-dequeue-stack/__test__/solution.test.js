3864f1068356a5046217225bf0c13ab9042471e8
Katy Robinson <katyorob@gmail.com>
2018-03-06 15:32:43 -0800

    whiteboarding completed

--- 17-enqueue-dequeue-stack/__test__/solution.test.js
+++ 17-enqueue-dequeue-stack/__test__/solution.test.js
@@ -1,28 +1,23 @@
 'use strict';
 
-const solution = require('../solution.js');
+const {StackNode, Stack, Queue} = require('../solution.js');
 require('jest');
 
-describe('Queue Module', function() {
-  describe('#enqueue test', function() {
-    it('should return inStack of 1, 2, 3', done => {
-      solution.newQ.enqueue(1)
-      solution.newQ.enqueue(2)
-      solution.newQ.enqueue(3)
-      let result = solution.inStack;
-      expect(result).toEqual([ 1, 2, 3 ]);
-      done();
+let q = new Queue();
+q.enqueue(1);
+q.enqueue(2);
+
+describe('Double Stack Queue', function() {
+  describe('#enqueue', function() {
+    it('should add a value to in stack', function() {
+      q.enqueue(3);
+      expect(q.inbox.top.val).toEqual(3);
     });
   });
-
-  describe('#dequeue test', function() {
-    it('should return an outStack of 3, 2, 1', done => {
-      solution.newQ.dequeue();
-      solution.newQ.dequeue();
-      solution.newQ.dequeue();
-      let result = solution.outStack;
-      expect(result).toEqual([ 3, 2, 1 ]);
-      done();
+  describe('#dequeue', function() {
+    it('should remove a value from the out stack', function() {
+      q.dequeue();
+      expect(q.outbox.top.val).toEqual(2);
     });
   });
 });
--- 17-enqueue-dequeue-stack/solution.js
+++ 17-enqueue-dequeue-stack/solution.js
@@ -1,33 +1,68 @@
 'use strict';
 
-var inStack = [];
-var outStack = [];
+class StackNode{
+  constructor(val) {
+    if(!val) return new Error('value not provided');
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack{
+  constructor(maxSize=1048) {
+    this.top = null;
+    this.size = 0;
+    this.maxSize = maxSize;
+  }
+
+  push(val) {
+    if(this.size === this.maxSize) throw new Error('stack overflow');
+    let node = new StackNode(val);
+
+    node.next = this.top;
+    this.top = node;
+    this.size++;
+    return this.top;
+  }
+
+  pop() {
+    let temp = this.top;
+    this.top = temp.next;
+    temp.next = null;
+    this.size--;
+
+    return temp.val;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+}
 
 class Queue{
   constructor() {
-    this.next = null;
-    this.length = 0;
+  this.inbox = new Stack;
+  this.outbox = new Stack;
   }
-  
+
   enqueue(val) {
-    inStack.push(val);
+    this.inbox.push(val);
   }
-  
+
   dequeue() {
-    if(outStack.length === 0) {
-      while(inStack.length > 0) {
-        let del = inStack.pop();
-        outStack.push(del);
+    if(this.outbox.size === 0) {
+      while(this.inbox.size > 0) {
+        let del = this.inbox.pop();
+        this.outbox.push(del);
       }
     }
+    this.outbox.pop();
   }
-};
-
-var newQ = new Queue();
+}
 
 module.exports = {
-  'Queue': Queue,
-  'inStack': inStack, 
-  'outStack': outStack,
-  'newQ': newQ,
+  'StackNode': StackNode,
+  'Stack': Stack,
+  'Queue': Queue
 }
